d3b6296ca2a4add116eb30d6ba24ace58f9fce46
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 00:16:21 +0530

    Added:class StartUC1.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,13 @@
+
 package snakesAndLadder;
 
-public class Welcome {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-         System.out.println("Welcome to SnakeAndLadder");
+public class StartLadderUC1 {
+	public static void main(String[] args){
+		int IS_START = 1;
+	    int game = (int) Math.floor(Math.random() * 6) % 2;
+		if (game == IS_START) {
+			System.out.println("Start the Game");
+		} else
+			System.out.println("Not Start the Game");
 	}
 }
