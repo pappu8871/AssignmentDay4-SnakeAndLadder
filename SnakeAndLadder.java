3e574a63a763137cdf4455f8864e7224948fc73a
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 00:28:48 +0530

    Added: class CheckOptionUC3.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,20 @@
 package snakesAndLadder;
-public class RollDiceUC2 {
+
+public class CheckOptionUC3 {
 	public static void main(String[] args) {
-		int empCheck = (int)Math.floor(Math.random() * 6) + 1;
-	    System.out.println(" printe Diec value: " + empCheck);
+		int IS_NO_PLAY = 0 ;
+		int IS_MOVES_HEAD = 1 ;
+		int IS_MOVES_BEHAND = 2 ;
+		
+		// TODO Auto-generated method stub
+		int dice = (int) Math.floor(Math.random() * 6) % 2;
+		if (dice == IS_NO_PLAY ) {
+		System.out.println("Stay in Same position ");
+		} else if (dice == IS_MOVES_HEAD) {
+			System.out.println("Ladder moves head by the number of position");
+		} else {
+			System.out.println("Snake moves behind by the number of position");
+		}
 	}
 }
 
