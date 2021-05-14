e1099e9673b44c96d95a43d9bbd1e456ac0022e9
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 00:23:36 +0530

    Added:class RollAndDice.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,13 +1,8 @@
-
 package snakesAndLadder;
-
-public class StartLadderUC1 {
-	public static void main(String[] args){
-		int IS_START = 1;
-	    int game = (int) Math.floor(Math.random() * 6) % 2;
-		if (game == IS_START) {
-			System.out.println("Start the Game");
-		} else
-			System.out.println("Not Start the Game");
+public class RollDiceUC2 {
+	public static void main(String[] args) {
+		int empCheck = (int)Math.floor(Math.random() * 6) + 1;
+	    System.out.println(" printe Diec value: " + empCheck);
 	}
 }
+
