d69ef28964fc25b5a208f76db29522185be5cf8d
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-15 00:34:20 +0530

    Added:class WinPositionUC4.java

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,20 +1,55 @@
 package snakesAndLadder;
 
-public class CheckOptionUC3 {
-	public static void main(String[] args) {
-		int IS_NO_PLAY = 0 ;
-		int IS_MOVES_HEAD = 1 ;
-		int IS_MOVES_BEHAND = 2 ;
-		
+public class WinPositionUC4 {
+	 public static final int NO_PLAY = 0;
+     public static final int LADDER = 1;
+     public static final int SNAKE = 2;
+     public static final int WINNING_POSITION = 100;
+	
+     public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int dice = (int) Math.floor(Math.random() * 6) % 2;
-		if (dice == IS_NO_PLAY ) {
-		System.out.println("Stay in Same position ");
-		} else if (dice == IS_MOVES_HEAD) {
-			System.out.println("Ladder moves head by the number of position");
-		} else {
-			System.out.println("Snake moves behind by the number of position");
-		}
+	   	  int Position = 0;
+	      int Roll = 0;
+	      int Start = 0;
+	      int Reach = 0;
+
+	      //int Value =(int) Math.floor((Math.random() * 6) + 1 );
+	      //System.out.println("Die Number Value: " + Value);
+
+	      //for (int Reach = 0; Reach < WINNING_POSITION; Reach++) {
+	        while (Reach <= WINNING_POSITION) {
+	         Reach++;
+
+	         int Value =(int) Math.floor((Math.random() * 6) + 1 );
+	         System.out.println("Die Number Value: " + Value);
+
+	         int option =(int) Math.floor((Math.random() * 10) % 3 );
+	         System.out.println("option number: " + option);
+
+	         switch (option) {
+	             case NO_PLAY:
+	                   Roll = 0;
+	                   break;
+	             case LADDER:
+	                   Roll = + Value;
+	                   break;
+	             case SNAKE:
+	                   Roll = -Value;
+	         }
+	        Position = (Position + Roll);
+	        if (Position < 0) {
+	             System.out.println("Current Position: " + Start );
+	             Reach = Start;
+	             System.out.println("Reached: " + Reach);
+	        }
+	        else {
+	             System.out.println("Current Position: " + Position);
+	             Reach = Position;
+	             System.out.println("Reached: " + Reach);
+	        }
+	     }
+	        //Reach = Position;
+	        System.out.println("Win: " + Reach);
 	}
 }
 
